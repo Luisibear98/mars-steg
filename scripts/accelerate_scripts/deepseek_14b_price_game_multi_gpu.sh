78ae5f293dd9138882c782a081d085834a1374c0
jskaf34 <joey.skaf@outlook.fr>
2025-03-08 13:45:47 +0100

    typo

--- scripts/accelerate_scripts/deepseek_14b_price_game_multi_gpu.sh
+++ scripts/accelerate_scripts/deepseek_14b_price_game_multi_gpu.sh
@@ -1,4 +1,4 @@
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file mars_steg/configs/accelerate/multi_gpu.yaml mars_steg.accelerate_training.py \
+ACCELERATE_LOG_LEVEL=info accelerate launch --config_file mars_steg/configs/accelerate/multi_gpu.yaml mars_steg/accelerate_training.py \
     experiments/b_sequential_price_game/1_collusion_with_neural_overseer.yaml \
     mars_steg/configs/config_deepseek_14b.yaml \
     scripts/1_testing_with_and_without_penalisation/with_penalise_overwrite.yaml \
