30b19ade4623c2ee43d17688d14a695fa7cb8cd1
jskaf34 <joey.skaf@outlook.fr>
2025-03-12 22:31:22 +0100

    typo

--- scripts/2_testing_pricing_game_on_length/run_pricing_game_deepseek_1pt5b_with_penalise.sh
+++ scripts/2_testing_pricing_game_on_length/run_pricing_game_deepseek_1pt5b_with_penalise.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 python -m \
-    mars_steg.train \
+   mars_steg.train_shortening \
     experiments/c_length_penalisation/1_collusion.yaml \
     mars_steg/configs/config_deepseek_1pt5b.yaml \
     mars_steg/configs/prompt/price_training_deepseek.yaml
