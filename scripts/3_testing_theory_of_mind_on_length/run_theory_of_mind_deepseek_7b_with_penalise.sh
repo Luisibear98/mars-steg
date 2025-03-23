9bc660cdfe9b32254dfdd52e52b5bee58a826e25
ConnorWatts <connorwatts@hotmail.com>
2025-03-23 21:54:46 +0000

    fix: 7b 14b fix

--- scripts/3_testing_theory_of_mind_on_length/run_theory_of_mind_deepseek_7b_with_penalise.sh
+++ scripts/3_testing_theory_of_mind_on_length/run_theory_of_mind_deepseek_7b_with_penalise.sh
@@ -2,6 +2,6 @@
 
 python -m \
    mars_steg.train_shortening \
-    experiments/c_length_penalisation/1_collusion.yaml \
+    experiments/c_length_penalisation/1_theory_of_mind.yaml \
     mars_steg/configs/config_deepseek_7b.yaml \
     mars_steg/configs/prompt/theory_of_mind_training_deepseek.yaml
