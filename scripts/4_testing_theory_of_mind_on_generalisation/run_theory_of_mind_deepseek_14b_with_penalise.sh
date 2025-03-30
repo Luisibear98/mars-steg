f43063525846801f7d9da439328f1ae8dc8f280c
P. Radmard <pr450@mirzakhani.eng.cam.ac.uk>
2025-03-30 17:44:49 +0100

    added 14b

---
+++ scripts/4_testing_theory_of_mind_on_generalisation/run_theory_of_mind_deepseek_14b_with_penalise.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+python -m \
+   mars_steg.train_generalisation \
+    experiments/d_generalisation/1_theory_of_mind.yaml \
+    mars_steg/configs/config_deepseek_14b.yaml \
+    experiments/d_generalisation/config_override.yaml \
+    mars_steg/configs/prompt/theory_of_mind_training_deepseek.yaml
