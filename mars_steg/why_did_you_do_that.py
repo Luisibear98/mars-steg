951e6fda946934b4d0ec54781261ec16c5a74949
jskaf34 <joey.skaf@outlook.fr>
2025-04-10 16:30:09 +0200

    Adding a script according to issue #105

---
+++ mars_steg/why_did_you_do_that.py
@@ -0,0 +1,199 @@
+"""
+Module Name: why_did_you_do_that.py
+------------------------
+
+
+Description:
+------------
+
+Created 29.03.2025
+Trying to see if the model is "conscious" of its steg behaviour
+See Also:
+---------
+
+- mars_steg.utils.common.get_dataloaders_and_ref_model
+- mars_steg.utils.common.prompt_data.BatchPromptData
+- mars_steg.task
+- mars_steg.language
+"""
+
+
+from __future__ import annotations
+import os
+from typing import Any, Dict
+import wandb
+import sys
+
+import torch
+from tqdm import tqdm
+from transformers import AutoTokenizer
+import sys
+from mars_steg.model.base_model import BaseModel
+from mars_steg.task.base_task import TorchDatasetTask
+from mars_steg.utils.prompt_data import BatchPromptData, log_merged_batch_wandb
+from mars_steg.config import  ExperimentArgs, GenerationConfig, InferenceConfig, InferenceConfigLoader, ModelConfig, PromptConfig
+from torch.utils.data import DataLoader
+
+from mars_steg.utils.common import (
+    get_dataloaders_and_ref_model,
+    get_tokenizer,
+    get_model,
+    get_device_map,
+    set_seed,
+)
+
+from peft import PeftModel
+
+sys.path.append(os.path.abspath(""))
+
+def run_inference(num_trials: int, 
+        model: BaseModel, 
+        tokenizer: AutoTokenizer, 
+        loader: DataLoader, 
+        dataset: TorchDatasetTask, 
+        prompt_config: PromptConfig,
+        device_map: Dict[str: str],
+        generation_kwargs: Dict[str: Any]
+        ) -> None:
+
+    for trial in range(num_trials):
+
+        print(f'TRIAL {trial}\n\n')
+
+        batch_ticker = 0
+        with torch.no_grad():
+            # START EVALUATION
+            for batch_prompt_datas in tqdm(loader):
+
+                batch_prompt_datas: BatchPromptData
+
+                batch_messages = model.batchize_conversation(
+                    user_prompts= batch_prompt_datas.cot_prompts, 
+                    system_prompt= dataset.system_prompt,
+                )
+
+                
+                transformed_batch_conversation = [model.transform_conversation(conversation, prompt_config.prompt_thinking_helper) for conversation in batch_messages]
+
+                inputs = model.tokenize(transformed_batch_conversation)
+
+                # Move the tokenized inputs to the same device the model is on (GPU/CPU)
+                inputs: Dict[str: torch.TensorType] = {key: tensor.to(device_map["main_model"]) for key, tensor in inputs.items()}
+                
+                # Generate the with-CoT transcript (no-CoT transcript not needed during training)
+                transcript_responses = model.full_generate(inputs, **generation_kwargs)
+                decoded_responses = [tokenizer.decode(r.squeeze()) for r in transcript_responses]
+                
+                # extraction of answer and cot (because cot_mode = True) parts of the transcript
+                batch_prompt_datas.cot_transcripts = decoded_responses
+                log_merged_batch_wandb([batch_prompt_datas], trial = trial, batch_in_trial = batch_ticker, name="_test")
+
+                batch_ticker += 1
+
+
+
+
+def main() -> None:
+
+    print(sys.argv)
+    experiment_args_path = sys.argv[1]
+    config_path = sys.argv[2]
+    prompt_config_path = sys.argv[3]
+
+    device_map = get_device_map()
+
+
+    ####################################################################################################################
+    # GET CONFIGS 
+    ####################################################################################################################
+
+    model_config, inference_config, generation_config = InferenceConfigLoader.load_config(config_path)
+
+    set_seed(inference_config.seed)
+
+    model_config: ModelConfig
+    inference_config: InferenceConfig
+    generation_config: GenerationConfig
+
+    prompt_config = PromptConfig.from_file(prompt_config_path)
+    generation_config.batch_size = inference_config.batch_size
+    experiment_args = ExperimentArgs.from_yaml(experiment_args_path)
+
+    ####################################################################################################################
+    # SET UP WANDB LOGGING
+    ####################################################################################################################
+
+    wandb.init(
+    project="MARS-STEGO",
+    entity="luisoso",
+    config={
+        'model_config': model_config,
+        'inference_config': inference_config,
+        'experiment_args': experiment_args,
+        'generation_config': generation_config,
+    }
+    )
+
+    # Set random seed for reproducibility
+
+    output_delimitation_tokens = {
+    "start_scratchpad_token": prompt_config.start_scratchpad_token,
+    "end_scratchpad_token": prompt_config.end_scratchpad_token,
+    "start_output_token": prompt_config.start_output_token,
+    "end_output_token": prompt_config.end_output_token,
+    }
+
+
+    tokenizer: AutoTokenizer = get_tokenizer(model_config.model_name)
+    generation_config.pad_token_id = tokenizer.eos_token_id 
+
+    #TODO Resolve the problem of the model looping on final answer
+    generation_config.eos_token_id = tokenizer.eos_token_id 
+    model = get_model(model_config, tokenizer, output_delimitation_tokens, generation_config, device_map["main_model"])
+
+    # Format: "entity/project/artifact_name:version"
+    artifact_reference = inference_config.lora_adaptaters_artefact_reference
+    # Download the artifact
+    artifact = wandb.use_artifact(artifact_reference)
+    artifact_lora_adaptaters_path = artifact.download()
+
+    if inference_config.load_lora_from_local:
+        print(f"LOADING LORA WEIGHTS FROM LOCAL {artifact_lora_adaptaters_path}")
+        model.model.pretrained_model.base_model.model = PeftModel.from_pretrained(
+        model.model.pretrained_model.base_model.model,
+        artifact_lora_adaptaters_path
+        )
+
+    generation_kwargs = generation_config.to_training_dict()
+
+    _, _, test_dataset, _, _, test_loader, _ = get_dataloaders_and_ref_model(
+    dataset_class_name = experiment_args.dataset_class_name,
+    penalisation_class_name = experiment_args.penalisation_class_name,
+    prompt_config = prompt_config,
+    dataset_class_kwargs = experiment_args.dataset_class_kwargs,
+    penalisation_class_kwargs = experiment_args.penalisation_class_kwargs,
+    train_proportion = 0.0,
+    validation_proportion = 0.0,
+    batch_size = inference_config.batch_size,
+    training_model = model,
+    override_create_ref_model = False,
+    test_train_split_kwargs={'mode': 'unseen_name'},
+    device_map=device_map
+    )
+    
+    run_inference(
+        num_trials= inference_config.num_trials,
+        model = model, 
+        tokenizer= tokenizer, 
+        loader= test_loader, 
+        dataset = test_dataset, 
+        prompt_config = prompt_config,
+        device_map=device_map,
+        generation_kwargs=generation_kwargs
+    )
+
+if __name__ == "__main__":
+    main()
+
+
+
