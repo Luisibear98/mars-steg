cad12fc8e9ca2b8318d953799eaab0dbf0c7413b
jskaf34 <joey.skaf@outlook.fr>
2025-04-30 13:35:40 +0200

    adapting luis test script

--- luis_test_script.sh
+++ luis_test_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CUDA_DEVICE=1
+CUDA_DEVICE=0
 SCRIPT=./scripts/4_testing_theory_of_mind_on_generalisation/test/test_theory_of_mind_deepseek_7b_with_penalise.sh
-MODEL_NAME=robust-field-29
+MODEL_NAME=MARS-STEGO_TRAIN_deepseek-ai_DeepSeek-R1-Distill-Qwen-7B_seed_42
 FIXED_ARG=0
 MAX=1592
 STEP=24
--- mars_steg/train_generalisation.py
+++ mars_steg/train_generalisation.py
@@ -530,7 +530,7 @@ if __name__ == "__main__":
     ####################################################################################################################
     
     if experiment_args.load_lora_from_wandb or (not model_config.lora and not train_config.create_ref_model): #Should We pass the project name as a param?
-        project_name = "MARS-STEGO TEST"
+        project_name = "MARS-STEGO DEBUG"
     else:
         project_name = "MARS-STEGO TRAIN"
 
