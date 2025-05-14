8e132e19af26e695e44bc8d3e29271ca19f04d3d
jskaf34 <joey.skaf@outlook.fr>
2025-05-14 11:51:38 +0200

    seed 336

--- luis_test_script.sh
+++ luis_test_script.sh
@@ -2,7 +2,7 @@
 
 CUDA_DEVICE=0
 SCRIPT=./scripts/4_testing_theory_of_mind_on_generalisation/test/test_theory_of_mind_deepseek_7b_with_penalise.sh
-MODEL_NAME=MARS-STEGO_TRAIN_GenPRM_GenPRM-7B_seed_294
+MODEL_NAME=MARS-STEGO_TRAIN_GenPRM_GenPRM-7B_seed_336
 FIXED_ARG=0
 MAX=96
 STEP=96
