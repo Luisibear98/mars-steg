8144ef2a1facea28d8b6c27006eb97a950d86ee5
jskaf34 <joey.skaf@outlook.fr>
2025-05-14 11:31:59 +0200

    seed 126

--- luis_test_script.sh
+++ luis_test_script.sh
@@ -2,12 +2,12 @@
 
 CUDA_DEVICE=0
 SCRIPT=./scripts/4_testing_theory_of_mind_on_generalisation/test/test_theory_of_mind_deepseek_7b_with_penalise.sh
-MODEL_NAME=MARS-STEGO_TRAIN_GenPRM_GenPRM-7B_seed_168
+MODEL_NAME=MARS-STEGO_TRAIN_GenPRM_GenPRM-7B_seed_126
 FIXED_ARG=0
-MAX=1592
+MAX=96
 STEP=96
 
-for ((i=96; i<=$MAX; i+=$STEP)); do
+for ((i=1536; i>=$MAX; i-=$STEP)); do
     echo "Running with argument: $i"
     CUDA_VISIBLE_DEVICES=$CUDA_DEVICE $SCRIPT $MODEL_NAME $FIXED_ARG $i
 done
